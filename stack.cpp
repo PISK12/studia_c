e04d19e7458e2f3c10a833b6229e67faf8b25e8d
PISK12 <piotr.k14@gmail.com>
2018-05-22 13:53:07 +0200

    Add files via upload

--- stack.cpp
+++ stack.cpp
@@ -1,36 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 struct Node{
     double value;
     struct Node *lower;
 };
 
-struct Node *top=NULL;
+struct Stack{
+    struct Node *top;
+};
 
-void push(double value){
+struct Stack *alloc(){
+    struct Stack *stack=(struct Stack*)malloc(sizeof(struct Stack));
+    stack->top=NULL;
+    return stack;
+};
+
+void clear(struct Stack *stack){
+    while (stack->top){
+        struct Node *node=stack->top;
+        stack->top=node->lower;
+        free(node);
+    }
+}
+
+void push(struct Stack *stack,double value){
     struct Node *node = (struct Node*)malloc(sizeof(struct Node));
     node->value=value;
-    node->lower=top;
-    top=node;
+    node->lower=stack->top;
+    stack->top=node;
 }
 
 
-double pop(){
-    double value=top->value;
-    struct Node *lower=top->lower;
-    free(top);
-    top=lower;
+double pop(struct Stack *stack){
+    struct Node *node=stack->top;
+    double value=node->value;
+    stack->top=node->lower;
+    free(node);
     return value;
 }
 
 
 int main(){
+    struct Stack *stack=alloc();
     double value;
     while (scanf("%lg",&value)==1){
-        push(value);
+        push(stack,value);
     }
-    while (top){
-        printf("%lg ",pop());
+    clear(stack);
+    while (stack->top){
+        printf("%lg ",pop(stack));
     }
 }
